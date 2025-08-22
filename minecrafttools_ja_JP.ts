e0a81c0ebd2ccf370f73454bd14cf709cc1d8b9e
TranslucentFoxHuman <tlfoxhuman@example.com>
2025-08-23 00:41:06 +0900

    v1.3.0 Released: Added villager generator.

--- minecrafttools_ja_JP.ts
+++ minecrafttools_ja_JP.ts
@@ -64,216 +64,340 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="200"/>
+        <location filename="mainwindow.ui" line="206"/>
         <source>Open Minecraft Wiki</source>
         <translation>Minecraft Wikiを開く</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="225"/>
+        <location filename="mainwindow.ui" line="243"/>
         <source>Search at Minecraft Wiki</source>
         <translation>Minecraft Wikiを検索</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="234"/>
+        <location filename="mainwindow.ui" line="252"/>
         <source>&amp;Search</source>
         <oldsource>Search</oldsource>
         <translation>検索(&amp;S)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="262"/>
+        <location filename="mainwindow.ui" line="280"/>
         <source>Mob Generator</source>
         <translation>Mobジェネレータ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="276"/>
+        <location filename="mainwindow.ui" line="294"/>
         <source>Mob Type :</source>
         <translation>モブの種類 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="284"/>
+        <location filename="mainwindow.ui" line="302"/>
+        <location filename="mainwindow.ui" line="321"/>
         <source>Horse</source>
         <translation>ウマ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="308"/>
+        <location filename="mainwindow.ui" line="307"/>
+        <location filename="mainwindow.ui" line="574"/>
+        <source>Villager</source>
+        <translation>村人</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="341"/>
         <source>Pattern :</source>
         <translation>パターン :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="316"/>
+        <location filename="mainwindow.ui" line="349"/>
         <source>None</source>
         <translation>無し</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="321"/>
+        <location filename="mainwindow.ui" line="354"/>
         <source>White</source>
         <comment>Pattern</comment>
         <translation>白足と頭の線</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="326"/>
+        <location filename="mainwindow.ui" line="359"/>
         <source>White Field</source>
         <translation>白のぶち</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="331"/>
+        <location filename="mainwindow.ui" line="364"/>
         <source>White Dots</source>
         <translation>白斑点</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="336"/>
+        <location filename="mainwindow.ui" line="369"/>
         <source>Black Dots</source>
         <translation>黒斑点</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="348"/>
+        <location filename="mainwindow.ui" line="381"/>
         <source>Color :</source>
         <translation>色 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="356"/>
+        <location filename="mainwindow.ui" line="389"/>
         <source>White</source>
         <comment>Color</comment>
         <translation>白</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="361"/>
+        <location filename="mainwindow.ui" line="394"/>
         <source>Creamy</source>
         <translation>クリーム色</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="366"/>
+        <location filename="mainwindow.ui" line="399"/>
         <source>Chestnut</source>
         <translation>栗色</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="371"/>
+        <location filename="mainwindow.ui" line="404"/>
         <source>Brown</source>
         <translation>茶色</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="376"/>
+        <location filename="mainwindow.ui" line="409"/>
         <source>Black</source>
         <translation>黒</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="381"/>
+        <location filename="mainwindow.ui" line="414"/>
         <source>Gray</source>
         <translation>灰色</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="386"/>
+        <location filename="mainwindow.ui" line="419"/>
         <source>Dark Brown</source>
         <translation>こげ茶</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="404"/>
+        <location filename="mainwindow.ui" line="437"/>
         <source>Health :</source>
         <translation>体力 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="427"/>
-        <location filename="mainwindow.ui" line="473"/>
-        <location filename="mainwindow.ui" line="510"/>
+        <location filename="mainwindow.ui" line="460"/>
+        <location filename="mainwindow.ui" line="506"/>
+        <location filename="mainwindow.ui" line="543"/>
         <source>Allow Cheats</source>
         <translation>チートを許可</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="444"/>
+        <location filename="mainwindow.ui" line="477"/>
         <source>Speed :</source>
         <translation>速度 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="484"/>
+        <location filename="mainwindow.ui" line="517"/>
         <source>Jump :</source>
         <translation>跳躍力 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="521"/>
+        <location filename="mainwindow.ui" line="554"/>
         <source>Tamed</source>
         <translation>飼い慣らす</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="528"/>
+        <location filename="mainwindow.ui" line="561"/>
         <source>Has saddle</source>
         <translation>鞍付き</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="540"/>
+        <location filename="mainwindow.ui" line="585"/>
+        <source>Type :</source>
+        <translation>種類 :</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="593"/>
+        <source>Plains</source>
+        <translation>平原</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="598"/>
+        <source>Desert</source>
+        <translation>砂漠</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="603"/>
+        <source>Jungle</source>
+        <translation>ジャングル</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="608"/>
+        <source>Savanna</source>
+        <translation>サバンナ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="613"/>
+        <source>Snowy</source>
+        <translation>降雪地帯</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="618"/>
+        <source>Swamp</source>
+        <translation>湿地帯</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="623"/>
+        <source>Taiga</source>
+        <translation>タイガ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="635"/>
+        <source>Profession :</source>
+        <translation>職業 :</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="643"/>
+        <source>Unemployed</source>
+        <translation>求職者</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="688"/>
+        <source>Librarian</source>
+        <translation>司書</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="703"/>
+        <source>Shepherd</source>
+        <translation>羊飼い</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="648"/>
+        <source>Armorer</source>
+        <translation>防具鍛冶</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="653"/>
+        <source>Butcher</source>
+        <translation>肉屋</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="658"/>
+        <source>Cartographer</source>
+        <translation>製図家</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="663"/>
+        <source>Cleric</source>
+        <translation>聖職者</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="668"/>
+        <source>Farmer</source>
+        <translation>農民</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="673"/>
+        <source>Fisherman</source>
+        <translation>釣り人</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="678"/>
+        <source>Fletcher</source>
+        <translation>矢師</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="683"/>
+        <source>Leatherworker</source>
+        <translation>革細工師</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="693"/>
+        <source>Nitwit</source>
+        <translation>無職</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="698"/>
+        <source>Mason</source>
+        <translation>石工</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="708"/>
+        <source>Toolsmith</source>
+        <translation>道具鍛冶</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="713"/>
+        <source>Weaponsmith</source>
+        <translation>武器鍛冶</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="725"/>
+        <source>Level :</source>
+        <translation>取引レベル :</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="751"/>
         <source>Output :</source>
         <translation>コマンド出力 :</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="567"/>
+        <location filename="mainwindow.ui" line="778"/>
         <source>&amp;Copy</source>
         <oldsource>Copy</oldsource>
         <translation>コピー(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="578"/>
+        <location filename="mainwindow.ui" line="789"/>
         <source>&amp;Generate</source>
         <oldsource>Generate</oldsource>
         <translation>生成(&amp;G)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="592"/>
+        <location filename="mainwindow.ui" line="803"/>
         <source>Memo</source>
         <translation>メモ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="615"/>
+        <location filename="mainwindow.ui" line="826"/>
         <source>&amp;Application</source>
         <oldsource>Application</oldsource>
         <translation>アプリケーション(&amp;A)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="622"/>
+        <location filename="mainwindow.ui" line="833"/>
         <source>&amp;Tools</source>
         <translation>ツール(&amp;T)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="639"/>
+        <location filename="mainwindow.ui" line="850"/>
         <source>&amp;About</source>
         <translation>このアプリケーションについて(&amp;A)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="648"/>
+        <location filename="mainwindow.ui" line="859"/>
         <source>E&amp;xit</source>
         <translation>終了(&amp;X)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="653"/>
+        <location filename="mainwindow.ui" line="864"/>
         <source>&amp;Coordinate</source>
         <translation>座標(&amp;C)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="658"/>
+        <location filename="mainwindow.ui" line="869"/>
         <source>&amp;Wiki</source>
         <translation>Wiki (&amp;W)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="663"/>
+        <location filename="mainwindow.ui" line="874"/>
         <source>&amp;Mob Generator</source>
         <translation>Mobジェネレーター(&amp;M)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="668"/>
+        <location filename="mainwindow.ui" line="879"/>
         <source>M&amp;emo</source>
         <translation>メモ(&amp;E)</translation>
-    </message>
-    <message>
-        <source>About</source>
-        <translation type="vanished">このアプリケーションについて</translation>
-    </message>
-    <message>
-        <source>Exit</source>
-        <translation type="vanished">終了</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="main.cpp" line="19"/>
+        <location filename="main.cpp" line="21"/>
         <source>https://minecraft.wiki/</source>
         <translation>https://ja.minecraft.wiki/</translation>
     </message>
